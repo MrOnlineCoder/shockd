ad255942b0771ff5ce142c3c5748349a72bf8c00
MrOnlineCoder <glinka1202@gmail.com>
2017-01-22 17:28:56 +0200

    Added Linux-specific defines

--- base.h
+++ base.h
@@ -11,6 +11,13 @@ typedef int socklen_t;
 
 #else // Hopefully, it will be Unix
 
+//ALL this stuff is not present in Linux :(
+#define INADDR_ANY      0x00000000
+#define INADDR_NONE     0xFFFFFFFF
+#define SOCKET_ERROR	(-1)
+#define closesocket     soclose
+#define SOCKET  int
+#define INVALID_SOCKET  -1
 typedef unsigned int SOCKET;
 
 #include <sys/socket.h>
